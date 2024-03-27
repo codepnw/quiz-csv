92db4435b30c415758a7f3a20769917717b8b05c
codepnw <idchokms01@outlook.com>
2024-03-27 23:21:33 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	csvFileName := flag.String("csv", "problems.csv", "a csv file in the format question, answer")
+	flag.Parse()
+
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		msg := fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFileName)
+		exit(msg)
+	}
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file.")
+	}
+	fmt.Println(lines)
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
