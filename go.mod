ef97a3bb7f86873a884d39365da336b848ceab09
codepnw <idchokms01@outlook.com>
2024-03-29 00:07:18 +0700

    read .csv quiz

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/codepnw/quiz-csv
+
+go 1.20
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -22,7 +23,22 @@ func main() {
 	if err != nil {
 		exit("Failed to parse the provided CSV file.")
 	}
-	fmt.Println(lines)
+
+	problems := parseLines(lines)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+
+		if answer == p.a {
+			correct++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
 }
 
 func parseLines(lines [][]string) []problem {
@@ -31,7 +47,7 @@ func parseLines(lines [][]string) []problem {
 	for i, line := range lines {
 		ret[i] = problem{
 			q: line[0],
-			a: line[1],
+			a: strings.TrimSpace(line[1]),
 		}
 	}
 	return ret
